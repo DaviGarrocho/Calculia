457e3544c3eeba22d8826ba3392484a5f29c7ea5
Davi Garrocho <davigarrocho11@gmail.com>
2025-08-05 22:24:46 -0300

    Musica
    Adicionei uma musiquinha baixinha em loop que fiz em IA

--- menu.js
+++ menu.js
@@ -1,9 +1,11 @@
 document.querySelectorAll('.g').forEach(div => {
     const audio = new Audio('assets/sound/hover.wav');
 
-    
     div.addEventListener('mouseenter', () => {
         audio.currentTime = 0; 
-        audio.play();         
+        audio.play();
     });
+
+    const musica = document.getElementById('menuMusic');
+    musica.volume = 0.15;
 });
