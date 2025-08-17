8828a5635dfb5ae0c3565ba3cc7b0119e23d65f7
Davi Garrocho <davigarrocho11@gmail.com>
2025-08-16 22:43:00 -0300

    modal

--- menu.js
+++ menu.js
@@ -5,7 +5,45 @@ document.querySelectorAll('.g').forEach(div => {
         audio.currentTime = 0; 
         audio.play();
     });
+    div.addEventListener('click', () => {
+        const modalId = div.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+        if (modal) {
+            modal.classList.add('active');
+        }
+    });
 
     const musica = document.getElementById('menuMusic');
-    musica.volume = 0.15;
+    if (musica) {
+        musica.volume = 0.15;
+    }
 });
+document.querySelectorAll('.modal-overlay').forEach(overlay => {
+    overlay.addEventListener('click', (e) => {
+        if (e.target === overlay) {
+            overlay.classList.remove('active');
+        }
+    });
+});
+
+
+document.querySelectorAll('.sair').forEach(botao => {
+    botao.addEventListener('click', () => {
+        const modal = botao.closest('.modal-overlay'); // sobe até o modal mais próximo
+        if (modal) {
+            modal.classList.remove('active');
+        }
+    });
+    });
+
+    
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        document.querySelectorAll('.modal-overlay.active').forEach(modal => {
+            modal.classList.remove('active');
+        });
+    }
+});
+
